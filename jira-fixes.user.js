00728d7b21f950b84710a4142c70225afe409b8e
Teddy Wing <git@teddywing.com>
2022-10-14 23:44:57 +0200

    Add license (GNU GPLv3+)

--- jira-fixes.user.js
+++ jira-fixes.user.js
@@ -8,6 +8,22 @@
 // @grant       none
 // ==/UserScript==
 
+// Copyright (c) 2022  Teddy Wing
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 // On the ticket page, the left content column is not necessarily focused on
 // page load. This means that the arrow keys and page up/down don't scroll the
 // left content column.
